3dc9649222e487b7d46a979740629eca19c8c6de
Bradley Hilton <graywolf336@craftyn.com>
2021-01-12 14:02:07 -0600

    Merge pull request #34 from Lawri-van-Buel/master
    Improve the rest API by having the Pivate Group functions.

--- rest/channels.go
+++ rest/channels.go
@@ -465,6 +465,15 @@ type OnlineChannelResponse struct {
 	Status
 	Online []models.OnlineChannel `json:"online"`
 }
+type GroupsResponse struct {
+	Status
+	models.Pagination
+	Groups []models.Channel `json:"groups"`
+}
+type GroupResponse struct {
+	Status
+	Group models.Channel `json:"group"`
+}
 
 // GetPublicChannels returns all channels that can be seen by the logged in user.
 //
@@ -481,6 +490,18 @@ func (c *Client) ChannelsList(params url.Values) (*ChannelsResponse, error) {
 func (c *Client) GetOnlineChannels(params url.Values) (*OnlineChannelResponse, error) {
 	response := new(OnlineChannelResponse)
 	if err := c.Get("channels.online", params, response); err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+// GetPrivateGroups returns all channels that can be seen by the logged in user.
+//
+// https://rocket.chat/docs/developer-guides/rest-api/groups/list
+func (c *Client) GetPrivateGroups() (*GroupsResponse, error) {
+	response := new(GroupsResponse)
+	if err := c.Get("groups.list", nil, response); err != nil {
 		return nil, err
 	}
 
@@ -863,4 +884,22 @@ func (c *Client) ChannelHistory(params url.Values) (*ChannelHistoryResponse, err
 	}
 
 	return response, nil
+}
+
+// GetGroupInfo get information about a group. That might be useful to update the usernames.
+//
+// https://rocket.chat/docs/developer-guides/rest-api/groups/info
+func (c *Client) GetGroupInfo(channel *models.Channel) (*models.Channel, error) {
+	response := new(GroupResponse)
+	switch {
+	case channel.Name != "" && channel.ID == "":
+		if err := c.Get("groups.info", url.Values{"roomName": []string{channel.Name}}, response); err != nil {
+			return nil, err
+		}
+	default:
+		if err := c.Get("groups.info", url.Values{"roomId": []string{channel.ID}}, response); err != nil {
+			return nil, err
+		}
+	}
+	return &response.Group, nil
 }
